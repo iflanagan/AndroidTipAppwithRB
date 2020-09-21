c14fae1c6b9c3a4a177fba7686a05c76b5931f00
Ian Flanagan <ianianf@yahoo.com>
2020-09-21 08:50:52 -0500

    Added Files to project.

---
+++ README.md
@@ -0,0 +1,136 @@
+# Android Tip Calculator
+
+This is a simple tip calculator app built for Android. The app is built with Java and uses the 
+native API.
+
+<img 
+    src="https://github.com/nadershamma/android-tip-calculator/blob/master/misc/android_tip_calc_app_1.png?raw=true" 
+    width="300px" height="auto" alt="Android Tip Calculator App Screenshot" align="center" />
+    
+## Main feature
+
+The app allows the user to input the amount owing on the bill and select how much they want to tip
+as a percentage of the bill.
+
+<img 
+    src="https://github.com/nadershamma/android-tip-calculator/blob/master/misc/android_tip_calc_demo_1.gif?raw=true" 
+    width="300px" height="auto" alt="Android Tip Calculator App Demo" align="center" />
+
+### Summary of features:
+
+    - A SeekBar which allows the user to choose the tip percenatge
+    - A GUI keyboard replacing the Android keyboard*
+    - Realtime update of the totals
+    - Input validation
+ 
+_*I could have simply used an EditText box which would have presented the inbuilt soft keyboard but 
+where is the fun in that?_
+
+## Implementation
+### Layout GUI
+
+The layout uses the ConstraintLayout to create a responsive layout. Within the ViewGroup there are:
+
+    - A TextView to present the Amount 
+    - A SeekBar to allow the user to set the percentage of the tip the want to pay between 0 and 30%   
+    - TextViews for the row labels and total amount containers
+    - Buttons for the GUI keyboard
+    - Guidelines to hold everything together
+  
+## Handling the SeekBar Event
+
+In order to handle the SeekBar change event an anonymous inner class which implements the 
+OnSeekBarChangeListener interface was created and set to the app SeekBar.
+
+```Java
+// Other important imports
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+
+// Getting the View SeekBar and assigning the seekBarListener
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        // [...]
+        
+        SeekBar percentSeekBar = findViewById(R.id.percentSeekBar);
+        percentSeekBar.setOnSeekBarChangeListener(seekBarListener);
+    }
+    
+    // [..]
+    
+    // Anonymous inner class
+    private final OnSeekBarChangeListener seekBarListener = new OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
+                // Do stuff with the SeekBar
+            }
+    
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+    
+            }
+    
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+    
+            }
+    };
+}
+```
+## GUI Keyboard
+The GUI keyboard was built using Android widget buttons. In order to capture the values they hold 
+when the user presses the button an `onClick()` listener had to be assigned to each button. There
+are two ways to approach this, either hardcode each button or dynamically cycle through each child
+View of the Layout ViewGroup and assign the onClick() listener to each one. For this implementation 
+the latter option was chosen.  
+
+```Java
+// Other important imports
+import android.widget.Button;
+import android.view.View.OnClickListener;
+import java.util.ArrayList;
+
+// Getting buttons from the main Layout and adding the onClick listener.
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        // [...]
+        private ArrayList<Button> buttons = new ArrayList<Button>();
+        
+        for (int i = 0; i < mainView.getChildCount(); i++) {
+                    if (mainView.getChildAt(i) instanceof Button) {
+                        buttons.add((Button) mainView.getChildAt(i));
+                    }
+                }
+        
+        for (Button button : buttons) {
+            button.setOnClickListener(buttonPressed);
+        }
+    }
+    
+    // [..]
+    
+    // Anonymous inner class
+    private final OnClickListener buttonPressed = new OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Button b = (Button) view;
+            // Do stuff with the button
+        }
+    };
+}
+```
+
+## User amount validation.
+The user amount was validated using regex to make sure that the input had no more than a single 
+`"."` and was a maximum of 2 decimal places. 
+
+`userAmountInput.toString().matches("(\\d*)(\\.?)(\\d{0,2})")` 
+# AndroidTipApp
---
+++ app/src/main/java/com/IanFlanagan/apps/tipcalculatorapp/MainActivity.java
@@ -0,0 +1,177 @@
+package com.IanFlanagan.apps.tipcalculatorapp;
+
+// The Activity Base class
+
+import android.support.v7.app.AppCompatActivity;
+
+// Used for saving state information
+import android.os.Bundle;
+
+import com.rollbar.android.Rollbar;
+
+//Android UI Classes
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Button;
+
+// Android UI event listener
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
+
+// Number formatting
+import java.text.NumberFormat;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final NumberFormat currencyFormat = NumberFormat.getCurrencyInstance();
+    private static final NumberFormat percentFormat = NumberFormat.getPercentInstance();
+
+    private double billAmount = 0.0;
+    private double percent = 0.15;
+    private TextView amountTextView;
+    private StringBuilder userAmountInput = new StringBuilder();
+    private TextView percentTextView;
+    private TextView tipTextView;
+    private TextView totalTextView;
+    private ViewGroup mainView;
+    private ArrayList<Button> buttons = new ArrayList<Button>();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Rollbar.init(this);
+        Rollbar.instance().error(new Exception("This is a test error From Ian's android project")); //remove this after initial testing
+
+        setContentView(R.layout.activity_main);
+
+        mainView = findViewById(R.id.mainLayout);
+
+        //Get the GUI text widgets
+        amountTextView = findViewById(R.id.amountTextView);
+        amountTextView.setText("Enter Bill Amount");
+        percentTextView = findViewById(R.id.percentTextView);
+        tipTextView = findViewById(R.id.tipTextView);
+        totalTextView = findViewById(R.id.totalTextView);
+        tipTextView.setText(currencyFormat.format(0));
+        totalTextView.setText(currencyFormat.format(0));
+
+        SeekBar percentSeekBar = findViewById(R.id.percentSeekBar);
+        percentSeekBar.setOnSeekBarChangeListener(seekBarListener);
+
+        for (int i = 0; i < mainView.getChildCount(); i++) {
+            if (mainView.getChildAt(i) instanceof Button) {
+                buttons.add((Button) mainView.getChildAt(i));
+            }
+        }
+
+        for (Button button : buttons) {
+            button.setOnClickListener(buttonPressed);
+        }
+    }
+
+    private void calculateBill() {
+        percentTextView.setText(percentFormat.format(percent));
+
+        double tip = billAmount * percent;
+        double total = billAmount + tip;
+
+        tipTextView.setText(currencyFormat.format(tip).toString());
+        totalTextView.setText(currencyFormat.format(total).toString());
+    }
+
+    private void resetBill() {
+
+        Rollbar.instance().error(new RuntimeException("Major issue with resetBill() method"));
+       // throws an exception to be sent to rollbar
+      throw new RuntimeException();
+
+
+        // comment the lines in and comment line 84 out to fix the problem
+       /* tipTextView.setText(currencyFormat.format(0));
+        totalTextView.setText(currencyFormat.format(0));*/
+    }
+
+    private void updateInputView(String amount) {
+        try {
+            userAmountInput.append(amount);
+            if (userAmountInput.toString().matches("(\\d*)")) {
+                amountTextView.setText(userAmountInput.toString());
+                billAmount = Double.parseDouble(userAmountInput.toString());
+                calculateBill();
+            } else if (userAmountInput.toString().matches("(\\d*)(\\.?)")) {
+                amountTextView.setText(userAmountInput.toString());
+                billAmount = Double.parseDouble(userAmountInput.toString().replace(".", "").trim());
+                calculateBill();
+            } else if (userAmountInput.toString().matches("(\\d*)(\\.?)(\\d{0,2})")) {
+                amountTextView.setText(userAmountInput.toString());
+                billAmount = Double.parseDouble(userAmountInput.toString());
+                calculateBill();
+            } else {
+                Toast.makeText(getApplicationContext(), "Invalid Amount", Toast.LENGTH_SHORT).show();
+                deleteInputView();
+            }
+        } catch (NumberFormatException e) {
+            Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_SHORT).show();
+        } catch (Exception e ) {
+
+            Toast.makeText(getApplicationContext(), "Error invalid input", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void deleteInputView() {
+        userAmountInput.deleteCharAt(userAmountInput.length() - 1);
+        amountTextView.setText(userAmountInput.toString());
+        if (userAmountInput.toString().matches("(\\d*)(\\.?)(\\d{0,2})")) {
+            billAmount = Double.parseDouble(userAmountInput.toString());
+            calculateBill();
+        }
+    }
+
+    private void clearInputView() {
+        userAmountInput.delete(0, userAmountInput.length());
+        amountTextView.setText("Enter Amount");
+        resetBill();
+    }
+
+    private final OnClickListener buttonPressed = new OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Button b = (Button) view;
+            if (b.getText().toString().equalsIgnoreCase("DEL")) {
+                if (userAmountInput.length() > 1) {
+                    deleteInputView();
+                } else {
+                    clearInputView();
+                }
+            } else if (b.getText().toString().equalsIgnoreCase("CLR")) {
+                clearInputView();
+            } else {
+                updateInputView(b.getText().toString());
+            }
+        }
+    };
+
+    private final OnSeekBarChangeListener seekBarListener = new OnSeekBarChangeListener() {
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
+            percent = progress / 100.0;
+            calculateBill();
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+
+        }
+    };
+}
